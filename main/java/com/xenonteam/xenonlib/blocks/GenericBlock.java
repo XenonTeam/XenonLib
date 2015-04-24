8613e46875040b39689680c2a48b96b1747cd92d
Pas341 <philipasanne@hotmail.com>
2015-04-25 00:42:52 +0200

    fix

--- main/java/com/xenonteam/xenonlib/blocks/GenericBlock.java
+++ main/java/com/xenonteam/xenonlib/blocks/GenericBlock.java
@@ -3,7 +3,8 @@
  */
 package com.xenonteam.xenonlib.blocks;
 
-import net.minecraft.block.BlockContainer;
+import net.minecraft.block.Block;
+import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
@@ -13,8 +14,7 @@ import net.minecraft.world.World;
  * @author philipas
  * 
  */
-public class GenericBlock extends BlockContainer {
-
+public class GenericBlock extends Block implements ITileEntityProvider {
 	
 	
 	protected GenericBlock(Material material, String UnlocalizedName) {
@@ -25,8 +25,10 @@ public class GenericBlock extends BlockContainer {
 	}
 	
 	@Override
-	public TileEntity createNewTileEntity(World worldIn, int meta) {
+	public TileEntity createNewTileEntity(World worldIn, int meta)
+	{
 		return null;
 	}
+
 
 }
