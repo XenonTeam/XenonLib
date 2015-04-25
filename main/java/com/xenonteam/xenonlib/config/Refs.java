e79fe04ab255bbf0632976b6ad713ba6f91884bd
tim4242 <tim4242@gmx.de>
2015-04-25 22:57:15 +0200

    update

---
+++ main/java/com/xenonteam/xenonlib/config/Refs.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.xenonteam.xenonlib.config;
+
+import java.util.ArrayList;
+
+/**
+ * @author tim4242
+ * @author philipas
+ *
+ */
+public class Refs
+{
+
+	public static final String MOD_NAME = "Xenon Lib";
+	public static final String MOD_ID = "xenon_lib";
+	public static final String MOD_VERSION = "0.0.1";
+	public static final String MOD_DEPS = "";
+	
+	public static final ArrayList<String> MOD_AUTHORS = new ArrayList<String>();
+	static
+	{
+		MOD_AUTHORS.add("philipas");
+		MOD_AUTHORS.add("tim4242");
+	}
+	
+}
---
+++ main/java/com/xenonteam/xenonlib/proxy/ServerProxy.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.xenonteam.xenonlib.proxy;
+
+/**
+ * @author tim4242
+ * @author philipas
+ *
+ */
+public class ServerProxy implements IXenonProxy
+{
+
+}
