4e1421d86d9bf1e465f0c5320915574072801104
tim4242 <tim4242@gmx.de>
2015-04-25 23:33:08 +0200

    update

--- main/java/com/xenonteam/xenonlib/registry/RegistryHelper.java
+++ main/java/com/xenonteam/xenonlib/registry/RegistryHelper.java
@@ -12,6 +12,7 @@ import java.util.List;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 /**
  * @author tim4242
@@ -26,6 +27,7 @@ public class RegistryHelper
 		
 		List<Field> toRegister = Arrays.asList(c.getDeclaredFields());
 		
+		String modid;
 		String unlocName;
 		
 		for(Field f : toRegister)
@@ -33,6 +35,7 @@ public class RegistryHelper
 			f.setAccessible(true);
 			if(f.isAnnotationPresent(Register.class))
 			{
+				modid = f.getAnnotation(Register.class).modid();
 				unlocName = f.getAnnotation(Register.class).unlocName();
 				
 				if(f.getDeclaringClass().isAssignableFrom(Block.class))
@@ -47,14 +50,14 @@ public class RegistryHelper
 						e.printStackTrace();
 					}
 					
-					
+					GameRegistry.registerBlock(b, modid + ":" + unlocName);
 					
 					return;
 				}
 				
 				if(f.getDeclaringClass().isAssignableFrom(Item.class))
 				{
-					TileEntity b = null;
+					Item b = null;
 					try
 					{
 						f.get(b);
@@ -64,20 +67,14 @@ public class RegistryHelper
 						e.printStackTrace();
 					}
 					
+					GameRegistry.registerItem(b, modid + ":" + unlocName);
+					
 					return;
 				}
 				
 				if(f.getDeclaringClass().isAssignableFrom(TileEntity.class))
 				{
-					TileEntity b = null;
-					try
-					{
-						f.get(b);
-					} 
-					catch (Exception e)
-					{
-						e.printStackTrace();
-					}
+					GameRegistry.registerTileEntity((Class<? extends TileEntity>) f.getDeclaringClass(), modid + ":" + unlocName);
 					
 					return;
 				}
