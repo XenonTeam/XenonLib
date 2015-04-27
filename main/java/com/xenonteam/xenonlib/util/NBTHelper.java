234207aebcf61935a5ed4119f161485506faad11
tim4242 <tim4242@gmx.de>
2015-04-27 02:13:53 +0200

    update

--- main/java/com/xenonteam/xenonlib/util/NBTHelper.java
+++ main/java/com/xenonteam/xenonlib/util/NBTHelper.java
@@ -18,12 +18,27 @@ import com.xenonteam.xenonlib.util.java.StorageHelper;
  */
 public class NBTHelper
 {
-
+	/**
+	 * Encodes an {@link java.lang.Object Object} into a {@link net.minecraft.nbt.NBTTagByteArray NBTTagByteArray} using {@link com.xenonteam.xenonlib.util.java.StorageHelper#serialize(java.lang.Object) StorageHelper.serialize(Object)}
+	 * 
+	 * @param obj The {@link java.lang.Object Object}
+	 * @return A {@link net.minecraft.nbt.NBTTagByteArray NBTTagByteArray}
+	 * @throws IOException
+	 */
 	public static NBTTagByteArray ObjectToNBT(Object obj) throws IOException
 	{
 		return new NBTTagByteArray(StorageHelper.serialize(obj));
 	}
 	
+	/**
+	 * 
+	 * Decodes an {@link net.minecraft.nbt.NBTTagByteArray NBTTagByteArray} to a {@link java.lang.Object Object} using {@link com.xenonteam.xenonlib.util.java.StorageHelper#deserialize(byte[]) StorageHelper.deserialize(byte[])}
+	 * 
+	 * @param b A {@link net.minecraft.nbt.NBTTagByteArray NBTTagByteArray}
+	 * @return A {@link java.lang.Object Object}
+	 * @throws ClassNotFoundException
+	 * @throws IOException
+	 */
 	public static Object NBTToObject(NBTTagByteArray b) throws ClassNotFoundException, IOException
 	{
 		return StorageHelper.deserialize(b.getByteArray());
