88d2d604b2d79c57d231cf496cbb22144fbd5be2
tim4242 <tim4242@gmx.de>
2015-05-08 22:02:30 +0200

    added docs to IGuiElement

---
+++ main/java/com/xenonteam/xenonlib/client/gui/element/GuiElementButton.java
@@ -0,0 +1,202 @@
+/**
+ * 
+ */
+package com.xenonteam.xenonlib.client.gui.element;
+
+import java.awt.Point;
+
+import net.minecraft.util.ResourceLocation;
+
+import com.xenonteam.xenonlib.client.gui.element.IGuiElement.IGuiActionHandler;
+import com.xenonteam.xenonlib.client.gui.element.IGuiElement.IGuiActionProvider;
+import com.xenonteam.xenonlib.client.gui.factory.IGuiFactory;
+
+/**
+ * @author tim4242
+ * @author philipas
+ *
+ */
+public class GuiElementButton implements IGuiActionProvider
+{
+
+	/* (non-Javadoc)
+	 * @see com.xenonteam.xenonlib.client.gui.element.IGuiElement#draw(com.xenonteam.xenonlib.client.gui.factory.IGuiFactory)
+	 */
+	@Override
+	public void draw(IGuiFactory factory)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.xenonteam.xenonlib.client.gui.element.IGuiElement#setXOff(int)
+	 */
+	@Override
+	public void setXOff(int x)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.xenonteam.xenonlib.client.gui.element.IGuiElement#setYOff(int)
+	 */
+	@Override
+	public void setYOff(int y)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.xenonteam.xenonlib.client.gui.element.IGuiElement#setPos(int, int)
+	 */
+	@Override
+	public void setPos(int x, int y)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.xenonteam.xenonlib.client.gui.element.IGuiElement#setPos(java.awt.Point)
+	 */
+	@Override
+	public void setPos(Point p)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.xenonteam.xenonlib.client.gui.element.IGuiElement#setPriority(int)
+	 */
+	@Override
+	public void setPriority(int priority)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.xenonteam.xenonlib.client.gui.element.IGuiElement#setHeight(int)
+	 */
+	@Override
+	public void setHeight(int height)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.xenonteam.xenonlib.client.gui.element.IGuiElement#setWidth(int)
+	 */
+	@Override
+	public void setWidth(int width)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.xenonteam.xenonlib.client.gui.element.IGuiElement#getXOff()
+	 */
+	@Override
+	public int getXOff()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.xenonteam.xenonlib.client.gui.element.IGuiElement#getYOff()
+	 */
+	@Override
+	public int getYOff()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.xenonteam.xenonlib.client.gui.element.IGuiElement#getPriority()
+	 */
+	@Override
+	public int getPriority()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.xenonteam.xenonlib.client.gui.element.IGuiElement#getHeight()
+	 */
+	@Override
+	public int getHeight()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.xenonteam.xenonlib.client.gui.element.IGuiElement#getWidth()
+	 */
+	@Override
+	public int getWidth()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.xenonteam.xenonlib.client.gui.element.IGuiElement#getParent()
+	 */
+	@Override
+	public IGuiElement getParent()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.xenonteam.xenonlib.client.gui.element.IGuiElement#getResource()
+	 */
+	@Override
+	public ResourceLocation getResource()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.xenonteam.xenonlib.client.gui.element.IGuiElement#setResource(net.minecraft.util.ResourceLocation)
+	 */
+	@Override
+	public void setResource(ResourceLocation loc)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.xenonteam.xenonlib.client.gui.element.IGuiElement.IGuiActionProvider#addActionHandler(com.xenonteam.xenonlib.client.gui.element.IGuiElement.IGuiActionHandler)
+	 */
+	@Override
+	public boolean addActionHandler(IGuiActionHandler handler)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.xenonteam.xenonlib.client.gui.element.IGuiElement.IGuiActionProvider#removeActionHandler(com.xenonteam.xenonlib.client.gui.element.IGuiElement.IGuiActionHandler)
+	 */
+	@Override
+	public boolean removeActionHandler(IGuiActionHandler handler)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
--- main/java/com/xenonteam/xenonlib/client/gui/element/IGuiElement.java
+++ main/java/com/xenonteam/xenonlib/client/gui/element/IGuiElement.java
@@ -10,54 +10,172 @@ import net.minecraft.util.ResourceLocation;
 import com.xenonteam.xenonlib.client.gui.factory.IGuiFactory;
 
 /**
+ * The core interface for all drawable gui elements
+ * 
  * @author tim4242
  * @author philipas
  * 
  */
-public interface IGuiElement 
+public interface IGuiElement
 {
-	
+
+	/**
+	 * Draws the element to the provided {@link IGuiFactory}.
+	 * 
+	 * @param factory The {@link IGuiFactory}
+	 */
 	public void draw(IGuiFactory factory);
 
+	/**
+	 * Sets the x offset
+	 * 
+	 * @param x new offset
+	 */
 	public void setXOff(int x);
-	
+
+	/**
+	 * Sets the y offset
+	 * 
+	 * @param y new y offset
+	 */
 	public void setYOff(int y);
-	
+
+	/**
+	 * Sets the position
+	 * 
+	 * @param x new x offset
+	 * @param y new y offset
+	 */
 	public void setPos(int x, int y);
-	
+
+	/**
+	 * Sets the position
+	 * 
+	 * @param p new position
+	 */
 	public void setPos(Point p);
-	
+
+	/**
+	 * Sets the priority (z order)
+	 * 
+	 * @param priority new priority
+	 */
 	public void setPriority(int priority);
-	
+
+	/**
+	 * Sets the height
+	 * 
+	 * @param height new height
+	 */
 	public void setHeight(int height);
-	
+
+	/**
+	 * sets the width
+	 * 
+	 * @param width new width
+	 */
 	public void setWidth(int width);
-	
+
+	/**
+	 * @return The x offset
+	 */
 	public int getXOff();
-	
+
+	/**
+	 * @return The y offset
+	 */
 	public int getYOff();
-	
+
+	/**
+	 * @return The priority (z order)
+	 */
 	public int getPriority();
-	
+
+	/**
+	 * @return The height
+	 */
 	public int getHeight();
-	
+
+	/**
+	 * @return The width
+	 */
 	public int getWidth();
-	
+
+	/**
+	 * @return {@link IGuiElement} the is the parent
+	 */
 	public IGuiElement getParent();
-	
+
+	/**
+	 * @return The {@link net.minecraft.util.ResourceLocation ResourceLocation}
+	 */
 	public ResourceLocation getResource();
-	
+
+	/**
+	 * Sets the {@link net.minecraft.util.ResourceLocation ResourceLocation}
+	 * 
+	 * @param loc new {@link net.minecraft.util.ResourceLocation ResourceLocation}
+	 */
 	public void setResource(ResourceLocation loc);
-	
-	
-	public interface IGuiActionHandler extends IGuiElement
+
+	/**
+	 * A version of {@link IGuiElement} meant to be interactive
+	 * 
+	 * @author tim4242
+	 * @author philipas
+	 *
+	 */
+	public interface IGuiActionProvider extends IGuiElement
 	{
-		
-		public void handleAction(int action, Object... args);
-		
+		/**
+		 * Registers a {@link IGuiActionHandler}
+		 * 
+		 * @param handler The handler to register
+		 * @return If it was successful
+		 */
+		public boolean addActionHandler(IGuiActionHandler handler);
+
+		/**
+		 * Deregisters a {@link IGuiActionHandler}
+		 * 
+		 * @param handler
+		 * @return
+		 */
+		public boolean removeActionHandler(IGuiActionHandler handler);
 	}
-	
-	public interface IGuiSpriteHandler extends IGuiElement{
+
+	/**
+	 * A interface to react to events
+	 * 
+	 * @author tim4242
+	 * @author philipas
+	 *
+	 */
+	public interface IGuiActionHandler
+	{
+		/**
+		 * The method that gets called when an action occurs
+		 * 
+		 * @param action The action
+		 * @param args The action specific arguments
+		 */
+		public void handleAction(int action, Object... args);
+	}
+
+	/**
+	 * A version of {@link IGuiElement} meant for static pictures
+	 * 
+	 * @author tim4242
+	 * @author philipas
+	 *
+	 */
+	public interface IGuiSpriteHandler extends IGuiElement
+	{
+		/**
+		 * Sets the sprite id
+		 * 
+		 * @param ID The sprite id
+		 */
 		public void setSpriteID(String ID);
 	}
 }
